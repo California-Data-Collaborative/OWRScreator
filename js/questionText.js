0faa9cc0244a5e22dfb3c549899d7ee8c3bfb0e4
christophertull <christophertull@users.noreply.github.com>
2017-07-26 16:14:45 -0700

    included new libraries

---
+++ js/questionText.js
@@ -0,0 +1,43 @@
+
+
+questionList = [
+"<b>Enter the Name of the Utility:</b>", 
+
+"<b>Enter the Bill Frequency:</b>"+
+"<br><br>"+
+"In the case of different billing frequencies for different customers, please choose the most commmon case.",
+
+"<b>Enter the Effective Date of this Rate Structure:</b>", 
+
+"<b>Select the Customer Classes Used by the Utility:</b>"+
+"<br><br>"+
+"Note: Not all rate structures will break neatly into these categories. "+
+"If your rate codes span multiple customer classes (e.g. single and multifamily are billed the same) "+
+"please provide the same pricing information for both classes."+
+"<br><br>"+
+"Alternately, there may be cases where "+
+"a single customer class (e.g. commercial) is broken up into a number of different rate codes. "+
+"In this case, please use your best judgement and provide information for the most common case.",
+
+"<b>Are customers in this class charged a fixed Service Charge?</b>",
+ 
+"<b>Does the Fixed Service Charge depend on the following:</b>",
+
+"<b>Are customers in this class charged a variable Commodity Charge?</b>", 
+
+"<b>What type of Variable Commodity Charge does this Customer Class Contain?</b> "+
+"<br><br>"+
+"Note that while a budget-based rate structure may include tiers, "+
+"we consider that a unique rate structure and have listed it separately.",
+
+"<b>Does this Uniform Commodity Charge Depend On Anything?</b>", 
+
+"<b>How Many Tiers are There?</b>",
+
+"<b>Do the Tier Levels Depend On Anything?</b>"+
+"<br><br>"+
+"For example", 
+
+"<b>What customer account attributes affect the price charged?</b>",
+
+"<b>Do the Tier Prices Depend On Anything?</b>"]
